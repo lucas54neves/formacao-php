2d1d06fa676c35e242f6453608c6e10a1cfc3bf2
Lucas Neves <lucas54neves@gmail.com>
2019-10-21 18:54:41 -0300

    Arquivos da segunda aula

---
+++ tipos.php
@@ -0,0 +1,7 @@
+<?php
+    $idade = 21;
+    $salario = 1000.30;
+
+    echo "idade = ".gettype($idade)."\n";
+    echo "idade = ".gettype($salario)."\n";
+?>
---
+++ variavel.php
@@ -0,0 +1,6 @@
+<?php
+    $idade = 21;
+    echo $idade."\n";
+    $idade = 22;
+    echo $idade."\n";
+?>
